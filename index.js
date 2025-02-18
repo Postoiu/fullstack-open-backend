441c2913229422d18894be40dc066f4dc7362c6d
Ovidiu Postoiu <postoiu.o@gmail.com>
2025-02-18 13:38:31 +0200

    Exercise 3.16

--- index.js
+++ index.js
@@ -29,26 +29,40 @@ app.use(
     })
 );
 
+const errorHandler = (error, request, response, next) => {
+    console.error(error.message);
+
+    if (error.name === 'CastError') {
+        return response.status(400).send({ error: 'malformatted id' });
+    } 
+
+    next(error);
+}
+
 app.get('/', (request, response) => {
     response.send('<h1>Phonebook API</h1>');
 })
 
-app.get('/info', (request, response) => {
-    Person.countDocuments({}).then(count => 
-        response.send(
-            `<p>Phonebook has info for ${count} people</p>
-            <p>${new Date()}</p>`
+app.get('/info', (request, response, next) => {
+    Person.countDocuments({})
+        .then(count => 
+            response.send(
+                `<p>Phonebook has info for ${count} people</p>
+                <p>${new Date()}</p>`
+            )
         )
-    )
+        .catch(error => next(error));
 })
 
-app.get('/api/persons', (request, response) => {
-    Person.find({}).then(persons => {
-        response.json(persons);
-    })
+app.get('/api/persons', (request, response, next) => {
+    Person.find({})
+        .then(persons => {
+            response.json(persons);
+        })
+        .catch(error => next(error))
 })
 
-app.post('/api/persons', (request, response) => {
+app.post('/api/persons', (request, response, next) => {
     const body = request.body;
 
     if(!body.name || !body.number) {
@@ -57,45 +71,45 @@ app.post('/api/persons', (request, response) => {
         })
     }
 
-    // if(persons.find(p => p.name === body.name)) {
-    //     return response.status(400).json({
-    //         error: 'Name must be unique'
-    //     })
-    // }
-
     const person = new Person({...body});
 
-    person.save().then(savedPerson => {
-        response.json(savedPerson);
-    })
-
+    person.save()
+        .then(savedPerson => {
+            response.json(savedPerson);
+        })
+        .catch(error => next(error))
 })
 
-app.get('/api/persons/:id', (request, response) => {
+app.get('/api/persons/:id', (request, response, next) => {
     Person.findById(request.params.id)
         .then(person => {
-            response.json(person);
-        });
-
-    // if(!person) {
-    //     return response.status(404).json();
-    // }
+            if(person) {
+                response.json(person);
+            } else {
+                response.status(404).end();
+            }
+        })
+        .catch(error => next(error));
 })
 
-app.delete('/api/persons/:id', (request, response) => {
+app.delete('/api/persons/:id', (request, response, next) => {
     Person.findByIdAndDelete(request.params.id)
         .then(result => {
             response.status(200).json(result);
         })
+        .catch(error => next(error))
 
 })
 
-app.put('/api/persons/:id', (request, response) => {
+app.put('/api/persons/:id', (request, response, next) => {
     Person.findByIdAndUpdate(request.params.id, request.body, { new: true })
         .then(result => {
             response.json(result);
         })
+        .catch(error => next(error))
 })
+
+app.use(errorHandler);
 
 app.listen(PORT, () => {
     console.log(`Server listening on port ${PORT}`);
